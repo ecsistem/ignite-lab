c17deaa4e383d73c678c624e599be56073d5775c
Edson Costa <edson.13.10.2004@gmail.com>
2022-06-23 19:07:31 -0300

    correção

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,8 +1,6 @@
 import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
 
 const defaultOptions: DefaultOptions = {
-
-
   watchQuery: {
     fetchPolicy: "no-cache",
     errorPolicy: "ignore",
@@ -14,7 +12,7 @@ const defaultOptions: DefaultOptions = {
 };
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ofnctk04c301w7en4ddxxo/master',
+  uri: "https://api-sa-east-1.graphcms.com/v2/cl4ofnctk04c301w7en4ddxxo/master",
   cache: new InMemoryCache(),
   defaultOptions: defaultOptions,
 });
