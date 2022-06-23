a2282fa23746cc7f5c229a450b3044448e834b0f
Edson Costa <edson.13.10.2004@gmail.com>
2022-06-23 18:01:09 -0300

    teste deploy

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,7 +3,7 @@ import { Event } from "../pages/Event";
 export function Router(){
     return(
         <Routes>
-            <Route path="/" element={<h1>Home teste</h1>}/>
+            <Route path="/" element={<Event />}/>
             <Route path="/event" element={<Event />}/>
             <Route path="/event/lesson/:slug" element={<Event />}/>
         </Routes>
