4b8e56abecc2bf3da25b019f5450bd9d524069af
Edson Costa <edson.13.10.2004@gmail.com>
2022-06-24 17:03:38 -0300

    correção de cor

--- src/components/CardLessons/index.tsx
+++ src/components/CardLessons/index.tsx
@@ -16,25 +16,36 @@ export function CardLessons(props: LessonProps) {
   return (
     <Link className="group" to={`/event/lesson/${props.slug}`}>
       <span className="text-gray-300">{AvaibleDateFormatted}</span>
-      <div className="rounded border border-gray-500 p-4 group-hover:border-green-500 group-active:bg-green-500">
-        <header className="flex items-center justify-between">
+      {/* <div className="rounded border border-gray-500 p-4 group-hover:border-green-500 group-active:bg-green-500">
+        <header className="flex items-center justify-between"> */}
           {isLessonAvaible ? (
+            <div className="rounded border border-gray-500 p-4 group-hover:border-green-500 group-active:bg-green-500">
+            <header className="flex items-center justify-between">
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2 group-active:text-white">
               <CheckCircle size={20} />
               Conteudo Liberado
             </span>
-          ) : (
-            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-              <Lock size={20} />
-              Em Breve
-            </span>
-          )}
-          <span className="text-xs text-white border  border-green-500 rounded py-[2px] px-2 font-bold">
+            <span className="text-xs text-white border  border-green-500 group-active:border-white rounded py-[2px] px-2 font-bold">
             {props.lessonType == "live" ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
-        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
+        <strong className="text-gray-200 group-active:text-white mt-5 block">{props.title}</strong>
       </div>
+          ) : (
+            <div className="rounded border border-gray-500 p-4 group-hover:border-orange-500 group-active:bg-orange-500 ">
+            <header className="flex items-center justify-between">
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2  group-active:text-white">
+              <Lock size={20} />
+              Em Breve
+            </span>
+            <span className="text-xs text-white border  border-orange-500 group-active:border-white rounded py-[2px] px-2 font-bold">
+            {props.lessonType == "live" ? "AO VIVO" : "AULA PRÁTICA"}
+          </span>
+        </header>
+        <strong className="text-gray-200 group-active:text-white mt-5 block">{props.title}</strong>
+      </div>
+          )}
+          
     </Link>
   );
 }
