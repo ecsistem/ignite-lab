e93fac3d96735cbe1bc3eace7d0c8b0fa83ce919
Edson Costa <edson.13.10.2004@gmail.com>
2022-06-24 17:19:40 -0300

    Hover and Active to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,10 +5,10 @@ export function Footer() {
     <footer className="w-full py-4 flex-col flex md:flex-row justify-between px-8 items-center bg-gray-900 border-t border-gray-500">
       <div className="flex gap-6 flex-col md:flex-row">
         <Logo />
-        <a href="">Rocketseat - Todos os direitos reservados</a>
+        <a className="text-gray-200 leading-relaxed hover:text-blue-500 active:text-blue-500" href="">Rocketseat - Todos os direitos reservados</a>
       </div>
       <div>
-        <a href="">Política de Privacidade</a>
+        <a className="text-gray-200 leading-relaxed hover:text-blue-500 active:text-blue-500" href="">Política de Privacidade</a>
       </div>
     </footer>
   );
